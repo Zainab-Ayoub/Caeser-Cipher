63129c24c9745380fe5ee701fb6cd3be770c1393
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-22 00:27:22 +0500

    end

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+from art import logo
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-
-def caesar(cipher_direction, shift_amount, start_text):
-    end_text = ''  
-    if cipher_direction == "decode":
-        shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+        position = alphabet.index(char)
         new_position = position + shift_amount
         end_text += alphabet[new_position]
-    print(f"The {direction}d text is {end_text}")
+    else: 
+        end_text += char 
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-caesar(cipher_direction = direction, shift_amount = shift, start_text = text)
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
