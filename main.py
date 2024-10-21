f5d53d3cc5dd515f25eed4dfe58a09ff17cded53
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-21 23:52:36 +0500

    art

--- main.py
+++ main.py
@@ -4,34 +4,16 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-def decrypt(cipher_text, shift_amount):
-  plain_text= ''
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The encoded text is {plain_text}")
+def caesar(cipher_direction, shift_amount, start_text):
+    end_text = ''  
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {direction}d text is {end_text}")
 
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-
-
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift) 
-elif direction == "decode":    
-    decrypt(cipher_text=text, shift_amount=shift) 
+caesar(cipher_direction = direction, shift_amount = shift, start_text = text)
